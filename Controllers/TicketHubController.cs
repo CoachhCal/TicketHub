821b4b723e842e885938c7f64ae7e41425115d00
CoachhCal <calvinj.murray@hotmail.com>
2025-03-26 16:46:10 -0300

    first commit

---
+++ Controllers/TicketHubController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketHubController : ControllerBase
+    {
+
+        private readonly ILogger<TicketHubController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        public TicketHubController(ILogger<TicketHubController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello GET from Ticket Hub Controller");
+        }
+
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+
+            if (string.IsNullOrEmpty(customer.CreditCard) || customer.CreditCard.Length != 16 || !customer.CreditCard.All(char.IsDigit))
+            {
+                return BadRequest("Invalid credit card number.");
+            }
+
+
+
+            return Ok("Hello " + customer.Name + " from Ticket Hub Controller");
+        }
+
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace TicketHub
+{
+    public class Customer
+    {
+
+        public int ConcertID { get; set; } = 0;
+        public required string Email { get; set; } = string.Empty;
+        public required string Name { get; set; } = string.Empty;
+        public required string Phone { get; set; } = string.Empty;
+        public required int  Quantity { get; set; } = 0;
+        public required string CreditCard { get; set; } = string.Empty;
+        public required string Expiration { get; set; } = string.Empty;
+        public required string SecurityCode { get; set; } = string.Empty;
+        public required string Address { get; set; } = string.Empty;
+        public required string City { get; set; } = string.Empty;
+        public required string Province { get; set; } = string.Empty;
+        public required string PostalCode { get; set; } = string.Empty;
+        public required string Country { get; set; } = string.Empty;
+
+    }
+}
